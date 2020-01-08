39a1f9c44ed9ec6221da0c65a8515bf2486ca969
SpericBlockchain <mekhakrishnan@spericorn.com>
2020-01-08 14:45:52 +0530

    fixes1

--- web3patch.js
+++ web3patch.js
@@ -1,17 +1,19 @@
-const fs = require('fs');
+const fs = require("fs");
 const f =
-  'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+  "node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
 
-fs.readFile(f, 'utf8', function(err, data) {
+fs.readFile(f, "utf8", function(err, data) {
   if (err) {
-    return console.log(err);
+    return err;
   }
   var result = data.replace(
     /node: false/g,
-    'node: {crypto: true, stream: true}'
+    "node: {crypto: true, stream: true}"
   );
 
-  fs.writeFile(f, result, 'utf8', function(err) {
-    if (err) return console.log(err);
+  fs.writeFile(f, result, "utf8", function(err) {
+    if (err) {
+      return err;
+    }
   });
 });
