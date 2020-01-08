3cbf96d6ea4c150d444773b113e6a03de771d53f
SpericBlockchain <mekhakrishnan@spericorn.com>
2020-01-08 14:28:12 +0530

    karma

--- src/app/Components/other/main-nav/main-nav.component.spec.ts
+++ src/app/Components/other/main-nav/main-nav.component.spec.ts
@@ -1,19 +1,19 @@
-import { LayoutModule } from '@angular/cdk/layout';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout'
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import {
   MatButtonModule,
   MatIconModule,
   MatListModule,
   MatSidenavModule,
   MatToolbarModule,
-} from '@angular/material';
+} from '@angular/material'
 
-import { MainNavComponent } from './main-nav.component';
+import { MainNavComponent } from './main-nav.component'
 
 describe('MainNavComponent', () => {
   let component: MainNavComponent;
-  let fixture: ComponentFixture<MainNavComponent>;
+  let fixture: ComponentFixture<MainNavComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,16 +27,16 @@ describe('MainNavComponent', () => {
         MatSidenavModule,
         MatToolbarModule,
       ]
-    }).compileComponents();
-  }));
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainNavComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(MainNavComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should compile', () => {
     expect(component).toBeTruthy();
-  });
-});
+  })
+})
